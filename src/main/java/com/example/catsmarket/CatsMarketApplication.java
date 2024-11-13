fb869f710d7ac1852f3a2cbf3c96e44a90ca9751
andyflight <erfy566@gmail.com>
2024-11-13 14:47:20 +0200

    init project

---
+++ src/main/java/com/example/catsmarket/CatsMarketApplication.java
@@ -0,0 +1,13 @@
+package com.example.catsmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatsMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatsMarketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/catsmarket/CatsMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.catsmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatsMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
